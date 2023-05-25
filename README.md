4798394d91ad18ff06bdc46a8af26228c87c77f6
Kaz <kkohno@gmail.com>
2023-05-24 23:07:13 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# LiteSQLite
+
+A thin wrapper library for android.database.sqlite.SQLiteOpenHelper, providing enhanced database operation capabilities through the use of Kotlin Coroutines.
---
+++ sample/src/main/kotlin/org/jugyo/litesqlite/sample/MainActivity.kt
@@ -0,0 +1,35 @@
+package org.jugyo.litesqlite.sample
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            MaterialTheme {
+                Screen()
+            }
+        }
+    }
+}
+
+@Composable
+private fun Screen() {
+    Text("TODO")
+}
